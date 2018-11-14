c50a955a0b75549eee5a56ad9419d12672cacd88
gallianojoaquin <37072671+gallianojoaquin@users.noreply.github.com>
2018-11-14 09:20:10 -0300

    Update ActualizarDatosClientes.java

--- Sorteo/src/ar/edu/ubp/das/src/sorteo/main/ActualizarDatosClientes.java
+++ Sorteo/src/ar/edu/ubp/das/src/sorteo/main/ActualizarDatosClientes.java
@@ -2,20 +2,25 @@ package ar.edu.ubp.das.src.sorteo.main;
 
 import java.util.Date;
 
+/**
+*
+* @author JOAQU√çN GALLIANO
+*
+*/
 public class ActualizarDatosClientes {
 	
 	public static void main(String []args) {
 
-		System.out.println( ">> Iniciando actualizaciÛn de clientes - " + new Date().toString() );
+		System.out.println( ">> Iniciando actualizaci√≥n de clientes - " + new Date().toString() );
 		
 		AdminDatosClientes datosClientes = AdminDatosClientes.getInstance();
 		String fechaHoy = "2018-08-10";
 		if(datosClientes.actualizarDatosClientes(null, fechaHoy)) {
-			System.out.println( "\n>> Actualizacion finalizada con Èxito." );
+			System.out.println( "\n>> Actualizacion finalizada con √©xito." );
 			System.exit(0);
 		}
 		
-		System.out.println( "\n>> ActualizaciÛn finalizada con error." );
+		System.out.println( "\n>> Actualizaci√≥n finalizada con error." );
 		
 	}
 }
